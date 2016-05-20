8c5c55576c43abd04542d716c72319f72ceae3c3
kurachi <paseri0406@gmail.com>
2016-05-20 13:56:14 +0900

    入力画面が形になった

--- 02/contact.php
+++ 02/contact.php
@@ -7,56 +7,62 @@
 </head>
 <body>
     <div id= page>
-    <form action="result.php" method="POST">
-        <table border="1">
-            <caption><h1>お問合せページ</h1></caption>
-        <tr>
-        <td>姓</td><td><input type="textbox" name="name1"></td>
-        </tr>
-        <tr>
-        <td>名</td><td><input type="textbox" name="name2"></td>
-        </tr>
-        <tr>
-        <td>性別</td><td><label><input type="radio" name="rdo" value="1" checked>男</label>
-            <label><input type="radio" name="rdo" value="2">女</label>
-            <label><input type="radio" name="rdo" value="3">不明</label></td>
-        </tr>
-        <tr>
-        <td>住所</td><td><input type="textbox" name="address"></td>
-        </tr>
-        <tr>
-        <td>電話番号</td><td><input type="textbox" name="num1" size="4"> -
-            <input type="textbox" name="num2" size="4"> -
-            <input type="textbox" name="num3" size="4"></td>
-        </tr>
-        <tr>
-        <td>メールアドレス</td><td><input type="textbox" name="mail_1"> @
-            <input type="textbox" name="mail_2"></td>
-        </tr>
-        <tr>
-        <td>どこで知りましたか？</td><td>
-            <label><input type="checkbox" name="chk[]" value="1">雑誌</label>
-            <label><input type="checkbox" name="chk[]" value="2">Web</label>
-            <label><input type="checkbox" name="chk[]" value="3">友人</label>
-            <label><input type="checkbox" name="chk[]" value="4">その他</label></td>
-        </tr>
-        <tr>
-        <td>質問カテゴリ選択</td><td>
-            <select name="question">
-                <option value="1">商品について</option>
-                <option value="2">サービスについて</option>
-                <option value="3">イベントについて</option>
-                <option value="4">その他</option>
-            </select></td>
-        </tr>
-        <tr>
-        <td>質問内容</td>
-            <td><textarea cols="50" rows="10" name="area">
-            </textarea></td>
-        </tr>
-    </table>
-        <input type="submit">
-    </form>
-</div>
+        <form action="result.php" method="POST">
+            <table border="1">
+                <caption><h1>お問合せページ</h1></caption>
+                <tr>
+                    <td >お名前</td>
+                    <td>姓: <input type="textbox" name="name1"><br><br>
+                        名: <input type="textbox" name="name2">
+                    </td>
+                </tr>
+                <tr>
+                    <td>性別</td><td><label><input type="radio" name="rdo" value="1" checked>男</label>
+                        <label><input type="radio" name="rdo" value="2">女</label>
+                        <label><input type="radio" name="rdo" value="3">不明</label></td>
+                </tr>
+                <tr>
+                    <td>住所</td>
+                    <td>〒<input type="textbox" maxlength="3" size="1"> -
+                        <input type="textbox" maxlength="4" size="1"><br>
+                        <input type="textbox" name="address"></td>
+                </tr>
+                <tr>
+                    <td>電話番号</td><td><input type="textbox" name="num1" size="4"> -
+                        <input type="textbox" name="num2" size="4"> -
+                        <input type="textbox" name="num3" size="4"></td>
+                </tr>
+                <tr>
+                    <td>メールアドレス</td><td><input type="textbox" name="mail_1"> @
+                        <input type="textbox" name="mail_2"></td>
+                </tr>
+                <tr>
+                    <td>どこで知りましたか？</td><td>
+                        <input type="hidden" name="chk[]" value="0">
+                        <label><input type="checkbox" name="chk[]" value="1">雑誌</label>
+                        <label><input type="checkbox" name="chk[]" value="2">Web</label>
+                        <label><input type="checkbox" name="chk[]" value="3">友人</label>
+                        <label><input type="checkbox" name="chk[]" value="4">その他</label></td>
+                </tr>
+                <tr>
+                    <td>質問カテゴリ選択</td><td>
+                        <select name="question">
+                            <option value="1">商品について</option>
+                            <option value="2">サービスについて</option>
+                            <option value="3">イベントについて</option>
+                            <option value="4">その他</option>
+                        </select></td>
+                </tr>
+                <tr>
+                    <td>質問内容</td>
+                    <td><textarea cols="50" rows="10" name="area">
+                    </textarea></td>
+                </tr>
+            </table>
+            <div id=s_btn>
+                <p><input id="submit_btn" type="submit"></p>
+            </div>
+        </form>
+    </div>
 </body>
 </html>
--- 02/result.php
+++ 02/result.php
@@ -1,16 +1,28 @@
-<?php
-echo "姓名： ". $_POST["name1"]. " ". $_POST["name2"]. "<br>";
-$gender_a = array("1" => "男", "2" => "女", "3" => "不明");
-echo "性別： ". $gender_a[$_POST["rdo"]]. "<br>";
-echo "電話番号: ". $_POST["num1"]. "-". $_POST["num2"]. "-". $_POST["num3"]. "<br>";
-echo "メールアドレス:". $_POST["mail_1"]. "@". $_POST["mail_2"]. "<br>";
-echo "どこで知りましたか？<br>";
-$check_a = array("1" => "雑誌", "2" => "Web", "3" => "友人", "4" => "その他");
-    foreach($_POST["chk"] as $value){
-        echo $check_a[$value]. ",";
-    }
-echo "<br>";
-$select_a = array("1" => "商品について", "2" => "サービスについて", "3" => "イベントについて", "4" => "その他");
-echo "質問カテゴリ<br>". $select_a[$_POST["question"]]. "<br>";
-echo "質問内容<br>". $_POST["area"];
-?>
+<!DOCTYPE html>
+<html lang="ja">
+    <head>
+        <meta charset="UTF-8">
+        <title>確認ページ</title>
+        <link rel="stylesheet" href="r_style.css">
+    </head>
+    <body>
+        <div id="word">
+            <?php
+                echo "姓名： ". $_POST["name1"]. " ". $_POST["name2"]. "<br>";
+                $gender_a = array("1" => "男", "2" => "女", "3" => "不明");
+                echo "性別： ". $gender_a[$_POST["rdo"]]. "<br>";
+                echo "電話番号: ". $_POST["num1"]. "-". $_POST["num2"]. "-". $_POST["num3"]. "<br>";
+                echo "メールアドレス:". $_POST["mail_1"]. "@". $_POST["mail_2"]. "<br>";
+                echo "どこで知りましたか？<br>";
+                $check_a = array("0" => "", "1" => "雑誌", "2" => "Web", "3" => "友人", "4" => "その他");
+                foreach($_POST["chk"] as $value){
+                    echo $check_a[$value]. ",";
+                }
+                echo "<br>";
+                $select_a = array("1" => "商品について", "2" => "サービスについて", "3" => "イベントについて", "4" => "その他");
+                echo "質問カテゴリ<br>". $select_a[$_POST["question"]]. "<br>";
+                echo "質問内容<br>". $_POST["area"];
+            ?>
+        </div>
+    </body>
+</html>
