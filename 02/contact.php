5243d029bfad05631a3c8cddd9ba190377873d9c
kurachi <paseri0406@gmail.com>
2016-05-23 18:26:22 +0900

    テキスト内背景の色変更

--- 02/contact.php
+++ 02/contact.php
@@ -21,8 +21,8 @@
             </div>
             <div id = sub>
                     <div class = "name">
-                        姓: <input type="textbox" name="name1" placeholder="田中" required><br><br>
-                        名: <input type="textbox" name="name2" placeholder="太郎" required><br>
+                        姓: <input type="textbox" name="name1" style="background-color:#EFEEF0;" placeholder="田中" required><br><br>
+                        名: <input type="textbox" name="name2" style="background-color:#EFEEF0;" placeholder="太郎" required><br>
                     </div>
                     <div class = "rdo">
                         <label><input type="radio" name="rdo" value="1" checked>男</label>
@@ -36,20 +36,20 @@
                             アパート・マンション名等<br>
                         </div>
                         <div id = addright>
-                            <input type="textbox" name="y_num1" maxlength="3" placeholder="123" size="1"> -
-                            <input type="textbox" name="y_num2" maxlength="4" placeholder="1241" size="1"><br>
-                            <input type="textbox" name="address1" placeholder="東京都なんちゃら" size="30"><br>
-                            <input type="textbox" name="address2" placeholder="○×マンション" size="30"><br>
+                            <input type="textbox" name="y_num1" style="background-color:#EFEEF0;" maxlength="3" placeholder="123" size="1"> -
+                            <input type="textbox" name="y_num2" style="background-color:#EFEEF0;" maxlength="4" placeholder="1241" size="1"><br>
+                            <input type="textbox" name="address1" style="background-color:#EFEEF0;" placeholder="東京都なんちゃら" size="30"><br>
+                            <input type="textbox" name="address2" style="background-color:#EFEEF0;" placeholder="○×マンション" size="30"><br>
                         </div>
                     </div>
                     <div class = "number">
-                        <input type="textbox" name="num1" maxlength="4" placeholder="123" size="4"> -
-                        <input type="textbox" name="num2" maxlength="4" placeholder="9999" size="4"> -
-                        <input type="textbox" name="num3" maxlength="4" placeholder="9999" size="4"><br>
+                        <input type="textbox" name="num1" maxlength="4" style="background-color:#EFEEF0;" placeholder="123" size="4"> -
+                        <input type="textbox" name="num2" maxlength="4" style="background-color:#EFEEF0;" placeholder="9999" size="4"> -
+                        <input type="textbox" name="num3" maxlength="4" style="background-color:#EFEEF0;" placeholder="9999" size="4"><br>
                     </div>
                     <div class = "mail">
-                        <input type="textbox" name="mail_1" placeholder="sanma0000" required> @
-                        <input type="textbox" name="mail_2" placeholder="sasasa.co.jp" required><br>
+                        <input type="textbox" name="mail_1" style="background-color:#EFEEF0;" placeholder="sanma0000" required> @
+                        <input type="textbox" name="mail_2" style="background-color:#EFEEF0;" placeholder="sasasa.co.jp" required><br>
                     </div>
                     <div class = "where">
                         <label><input type="checkbox" name="chk[]" value="1">雑誌</label>
@@ -58,7 +58,7 @@
                         <label><input type="checkbox" name="chk[]" value="4">その他</label><br>
                     </div>
                     <div class = "select">
-                        <select name="question">
+                        <select name="question" style="background-color:#EFEEF0;">
                             <option value="0">選択してね</option>
                             <option value="1">商品について</option>
                             <option value="2">サービスについて</option>
@@ -67,7 +67,7 @@
                         </select><br>
                     </div>
                 <div class = "value">
-                    <textarea cols="70" rows="10" name="area" placeholder="ご意見・ご感想など" required></textarea>
+                    <textarea cols="70" rows="10" name="area" style="background-color:#EFEEF0;" placeholder="ご意見・ご感想など" required></textarea>
                 </div>
             </div>
             <div id=s_btn>
